74d5d93ca07be1c310d683bbbd27915b8ee5359f
OkaOka_520 <85399297+OkaOka-502@users.noreply.github.com>
2021-06-24 09:45:38 +0700

    Beberapa Cara Menampilkan Text Ke Layar

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
   echo "Cici Oktaviani<br>";
   print "Xl Rekayasa Perangkat Lunak<br>";
-  echo "<br>";
   var_dump ('11th Oct  2003');
+  echo "<br>";
   print_r ('Tasikmalaya City, West Java');
 
       
