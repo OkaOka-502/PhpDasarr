80ffdc8bf69fd23d7261d5081a1034a2c49891b8
OkaOka_520 <85399297+OkaOka-502@users.noreply.github.com>
2021-06-24 09:58:14 +0700

    Variabel Dalam Php dan Concatenation (penggabung string
    *

---
+++ variabel.php
@@ -0,0 +1,11 @@
+<?php
+
+/* Aturan penulisan Variabel dalam php
+* Penulisan di awali dengan tanda '$'
+* Tidak boleh diawali dengan angka tapi bisa menggunakan angka
+* Bersifat case sensitif
+* Tidak boleh menggunakan spasi
+*/
+
+
+?>
