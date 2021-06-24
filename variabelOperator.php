757a3474beff6a854f605282e083b934bccd3cb2
OkaOka_520 <85399297+OkaOka-502@users.noreply.github.com>
2021-06-24 15:15:06 +0700

    Menambahkan cara menampilkan output

--- variabelOperator.php
+++ variabelOperator.php
@@ -1,5 +1,19 @@
 <?php
 
+/*Beberapa Cara untuk menampilkan output ke layar dengan:
+* echo
+* print
+* var_dump(menampilkan tipe data dan ukuran data biasanya untuk array )
+* print_r (biasanya untuk array)
+*/
+
+  echo "Cici Oktaviani<br>";
+  print "Xl Rekayasa Perangkat Lunak<br>";
+  var_dump ('11th Oct  2003');
+  echo "<br>";
+  print_r ('Tasikmalaya City, West Java');
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 /* Aturan penulisan Variabel dalam php
 * Penulisan di awali dengan tanda '$'
 * Tidak boleh diawali dengan angka tapi bisa menggunakan angka
@@ -7,14 +21,18 @@
 * Tidak boleh menggunakan spasi
 * etc.
 */
+
 /*Penulisan syntax php
 * Php didalam html
 * Htmml didalam Php
 */
-// Contoh Penulisan Variabel
-$name = "Oka";
-echo "Hello My Name is $name <br>";
 
+// Contoh Penulisan Variabel
+
+   $name = "Oka";
+   echo "Hello My Name is $name <br>";
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 /* Operator
 * Operator Aritmatika
 * Operator Penugasan atau Assignment
@@ -24,14 +42,17 @@ echo "Hello My Name is $name <br>";
 * Operator Bitwise
 * Operator Ternary.
 */
-$x = 10;
-$y = 20;
-echo $x * $y + $X;
-echo "<br>";
 
+  $x = 10;
+  $y = 20;
+  echo $x * $y + $X;
+  echo "<br>";
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Concatenation/contat/penggabung string ." ".
-$f_n = 'Cici';
-$l_n = 'Oktaviani';
-echho $f_n." ".$l_n;
+
+  $f_n = 'Cici';
+  $l_n = 'Oktaviani';
+  echo $f_n." ".$l_n;
 
 ?>
