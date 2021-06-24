6e8bdb7f8ae783fb55fbc7ad74fa4565ed9edfb4
OkaOka_520 <85399297+OkaOka-502@users.noreply.github.com>
2021-06-24 09:40:31 +0700

    Berbagai Cara Menampilkan Text Ke Layar
    Standar output
    * echo,print
    * var_dump, print_r (Biasanya Untuk Array)
    var_dump akan menampilkan tipe data dan panjang data/ukuran data

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+  echo "Cici Oktaviani<br>";
+  print "Xl Rekayasa Perangkat Lunak<br>";
+  echo "<br>";
+  var_dump ('11th Oct  2003');
+  print_r ('Tasikmalaya City, West Java');
+
+      
+?>
+    
