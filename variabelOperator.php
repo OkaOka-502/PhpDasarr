45cae76a3a1a92b3db1b6d989b20b67518595ead
OkaOka_520 <85399297+OkaOka-502@users.noreply.github.com>
2021-06-24 10:25:55 +0700

    Variabel,Operator,Concatenation.

---
+++ variabelOperator.php
@@ -0,0 +1,37 @@
+<?php
+
+/* Aturan penulisan Variabel dalam php
+* Penulisan di awali dengan tanda '$'
+* Tidak boleh diawali dengan angka tapi bisa menggunakan angka
+* Bersifat case sensitif
+* Tidak boleh menggunakan spasi
+* etc.
+*/
+/*Penulisan syntax php
+* Php didalam html
+* Htmml didalam Php
+*/
+// Contoh Penulisan Variabel
+$name = "Oka";
+echo "Hello My Name is $name <br>";
+
+/* Operator
+* Operator Aritmatika
+* Operator Penugasan atau Assignment
+* Operator Increment & Decrement
+* Operator Relasi atau pembanding
+* Operator Logika
+* Operator Bitwise
+* Operator Ternary.
+*/
+$x = 10;
+$y = 20;
+echo $x * $y + $X;
+echo "<br>";
+
+// Concatenation/contat/penggabung string ." ".
+$f_n = 'Cici';
+$l_n = 'Oktaviani';
+echho $f_n." ".$l_n;
+
+?>
